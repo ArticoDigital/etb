6cc6f6b48460276c181025b8798d970cd1f85d38
MikeCamacho <michaelsamuelca@gmail.com>
2017-06-21 14:19:23 -0500

    touch click
    ciudad tocuh click

--- js/ciudad.js
+++ js/ciudad.js
@@ -15,13 +15,40 @@ var wh = $(window).height();
 	$(window).resize(function(){
    ww = $(window).width()
 });
+
 $('#Parallax-content').bind('touchmove',function(e){
+	var documentClick = true;
+	$('.item-2').on('click touchend', function(event) {
+    if (event.type == "click") documentClick = true;
+    if (documentClick){
+        goToPage(4);
+        document.location.hash = "pagina_" + page; 
+    }
+ 	$('.item-3').on('click touchend', function(event) {
+    if (event.type == "click") documentClick = true;
+    if (documentClick){
+        goToPage(5);
+        document.location.hash = "pagina_" + page; 
+    }
+    $('.item-5').on('click touchend', function(event) {
+    if (event.type == "click") documentClick = true;
+    if (documentClick){
+        goToPage(6);
+        document.location.hash = "pagina_" + page; 
+    }
+    $('.item-6').on('click touchend', function(event) {
+    if (event.type == "click") documentClick = true;
+    if (documentClick){
+        goToPage(7);
+        document.location.hash = "pagina_" + page; 
+    }
+ 	});
       e.preventDefault(); 
        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-	   var amountMovedXm = touch.pageX *  ( ww - 3223 ) / ww  ;  
-	   var amountMovedYm = (touch.pageY * ( wh - 974 ) / wh);
+	   var amountMovedXm = touch.pageX *  ( ww - 980 ) / ww  ;  
+	   var amountMovedYm = (touch.pageY * ( wh - 640 ) / wh);
 	   
-	   var amountMovedX1m = touch.pageX *  ( ww - 3223 ) / (ww/1.1)  ; 
+	   var amountMovedX1m = touch.pageX *  ( ww - 980 ) / (ww/1.1)  ; 
 	
 	   $(this).css('background-position', amountMovedXm + 'px ');
 	   $('.items').css('transform', 'translateX('+ amountMovedX1m + 'px)');
